6b453bb881a4a0b88b839996389b6b10863f827b
Aspaceman527 <92396467+Aspaceman527@users.noreply.github.com>
2022-02-24 10:04:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+	var y = Math.floor(Math.random() * 100 + 1);
+	
+	var guess = 1;
+	
+	document.getElementById("submitguess").onclick = function(){
+	
+var x = document.getElementById("guessField").value;
 
+if(x == y)
+{	
+	alert("CONGRATULATIONS!!! YOU GUESSED IT RIGHT IN "
+			+ guess + " GUESS ");
+}
+else if(x > y)
+{	
+	guess++;
+	alert("TRY AGAIN");
+}
+else
+{
+	guess++;
+	alert("TRY AGAIN")
+}
+}
