e929ea4159fe80b0d60c37ceb45992f33f3f501f
Aspaceman527 <92396467+Aspaceman527@users.noreply.github.com>
2022-03-22 13:37:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-	var z = 2;
 	var y = Math.floor(Math.random() * 100 + 1);
 	var guess = 1;
 	document.getElementById("submitguess").onclick = function(){
@@ -9,7 +8,37 @@ if(x == y)
 {	
 	alert("CONGRATULATIONS!!! YOU GUESSED IT RIGHT IN "
 			+ guess + " GUESSES ");
-	var z = 3;
+	const name = window.prompt("nickname");
+const score = guess
+
+async function addScore() {
+  await ADD(name.value, score.value);
+  DISPLAY();
+  name.value = '';
+  score.value = '';
+  name.focus();
+}
+
+name.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') score.focus();
+});
+
+score.addEventListener('keypress', async (e) => {
+  if (e.key === 'Enter') addScore();
+});
+
+submit.addEventListener('click', async () => {
+  addScore();
+});
+
+refresh.addEventListener('click', () => {
+  DISPLAY();
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+  DISPLAY();
+});
+
 }
 else if(x > y)
 {	
